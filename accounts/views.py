34b1b27913a333cb43cfacbe7a0d9ec612b99581
masande-99 <gontyelenimasande@gmail.com>
2021-07-22 10:54:05 +0200

    added a templates

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'account/dashboard.html')
 
 
 def products(request):
