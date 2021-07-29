93d48ed9f88d7117fe74e6518befbc9363563273
masande-99 <gontyelenimasande@gmail.com>
2021-07-29 12:37:14 +0200

    Made some changes

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,19 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from .models import *
+
+
 def home(request):
     return render(request, 'account/dashboard.html')
 
 
 def products(request):
-    return render(request, 'account/products.html')
+    product = models.Product.objects.all()
+    print(product)
+
+    return render(request, 'account/products.html', {'Products': product})
 
 
 def customer(request):
     return render(request, 'account/customer.html')
-
